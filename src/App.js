2158c1a147877915ef2f8b1ed66b481719ea2c98
lixi <1491812683@qq.com>
2019-11-10 22:27:23 +0800

    Simple move

--- src/App.js
+++ src/App.js
@@ -16,35 +16,48 @@ const svgs = [svg1, svg2, svg3];
 function App() {
   let drawEl = useRef(null);
   let [layer, setLayer] = useState([]);
-  useEffect(() => {
-    axios.get(svg1).then(({ data }) => {
-
+  const appendSvgNode = (filePath) => {
+    axios.get(filePath).then(({ data }) => {
       // load svg file
-      let draw = SVG().addTo(drawEl.current);
+      let draw = SVG('#canvas svg');
       draw.svg(data, true);
-
       // define svg.js instance
       draw = SVG(drawEl.current).get(0);
-
-      // TODO response size of svg
-      draw.attr({ height: 400, width: 300 })
       setLayer(draw.children());
-      // console.log('draw.children(): ', draw.children());
+
+      draw.children().forEach(c => {
+        if (c.animate) {
+          c.animate().dx(20)
+        }
+        if (c.click) {
+          c.click(function () {
+            this.fill({ color: '#f06' })
+          })
+        }
+
+      })
     })
+  }
+  useEffect(() => {
+    // init canvas size
+    let height = drawEl.current.parentNode.clientHeight;
+    drawEl.current.style.height = height * 0.9 + 'px';
+    drawEl.current.style.width = height * 0.9 + 'px';
+    appendSvgNode(svg1)
   }, [])
   return (
-    <div style={{ display: 'flex', height: '100vh', flexDirection: 'column' }}>
-      <div style={{ flex: '1', display: 'flex' }}>
+    <div>
+      <div style={{ display: 'flex', height: '70vh' }}>
         <div style={{ flex: '0.2' }} className='assets'>
           <div className='menu'>
             <Icon type='upload' />
             {menuIcons.map(icon => <Icon type={icon} key={icon} />)}
           </div>
           <div className='imgs'>
-            {svgs.map(svg => <img src={svg} key={svg} />)}
+            {svgs.map(svg => <img src={svg} key={svg} onClick={() => appendSvgNode(svg)} />)}
           </div>
         </div>
-        <div style={{ flex: '0.8' }} className='handle'>
+        <div style={{ flex: 0.8 }} className='handle'>
           <header>
             <div>
               <Icon type='arrow-left' />
@@ -54,14 +67,18 @@ function App() {
             <div>画布设置</div>
             <div><Icon type='play-circle' /></div>
           </header>
-          <div className='canvas' ref={drawEl}></div>
+          <div className='canvas-container'>
+            <div className='canvas' ref={drawEl} id='canvas'>
+              <svg></svg>
+            </div>
+          </div>
         </div>
       </div>
-      {/* TODO set fixed height */}
-      <div style={{ height: '200px', overflowY: 'scroll' }} className='timeline'>
+      <div style={{ height: '30vh', overflowY: 'scroll' }} className='timeline'>
         <List
           bordered
           dataSource={layer}
+          style={{ height: '100%' }}
           renderItem={item => (<List.Item>{item.type}</List.Item>)}
         />
       </div>
